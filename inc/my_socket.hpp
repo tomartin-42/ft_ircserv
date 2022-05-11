e88b69961cea433853643edc7cf8720b1362bc69
tomartin-42 <tomasmartinezcurz@gmail.com>
2022-05-11 13:19:18 +0200

    work in polls

--- inc/my_socket.hpp
+++ inc/my_socket.hpp
@@ -2,12 +2,15 @@
 # define _MY_SOCKET_HPP_
 
 #include <vector>
+#include <queue>
+#include <string>
 #include <arpa/inet.h>
 #include <fcntl.h> // To fcntl function
 #include <sys/socket.h> // To socket
 #include <netinet/in.h> // To struct sockaddr_in
 #include <sys/types.h>
 #include <poll.h> // To poll
+#include <fcntl.h> // To fcntl
 
 class	my_socket
 {
@@ -16,6 +19,9 @@ class	my_socket
 		std::vector<pollfd>	poll_fds;
 		struct sockaddr_in	data_socket;
 		int					data_socket_len; //Need to accept function
+		std::queue<std::string>	msg_queue;
+
+		void				scan_fds();
 
 	public:
 		my_socket(const int port);
--- src/my_socket.cpp
+++ src/my_socket.cpp
@@ -1,5 +1,26 @@
 #include "my_socket.hpp"
 
+void	my_socket::scan_fds()
+{
+	std::vector<pollfd>::iterator	it;
+
+	for(it = this->poll_fds.begin(); it != this->poll_fds.end(); it++)
+	{
+		if(it->revents & POLLIN)
+		{
+			if(it->fd == this->socket_fd)
+			{
+				int	new_fd;
+
+				new_fd = accept(this->socket_fd, (struct scokaddr_in *) &(this->data_socket), (socklen_t *) &(this->data_socket_len));
+				poll_fds.push_back(pollfd());
+				poll_fds.back().fd = new_fd;
+				poll_fds.back().events = POLLIN;
+			}
+		}
+	}
+}
+
 my_socket::my_socket(const int port)
 {
 	this->data_socket.sin_family = AF_INET;
@@ -10,11 +31,17 @@ my_socket::my_socket(const int port)
 
 void	my_socket::init_socket()
 {
+	int	enable; //need to enable setsockop values
+
+	enable = 1; //need to enable setsockopt values
 	this->socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+	setsockopt(this->socket_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &enable, sizeof(enable));
+	fcntl(this->socket_fd, F_SETFL, O_NONBLOCK);
 	bind(this->socket_fd, (struct sockaddr *) &(this->data_socket), sizeof(data_socket));
 	listen(this->socket_fd, 10);
 	poll_fds.push_back(pollfd());
 	poll_fds.back().fd = this->socket_fd;
 	poll_fds.back().events = POLLIN;
 	poll((struct pollfd *) &(this->poll_fds[0]), this->poll_fds.size(), 50000);
+	this->scan_fds();
 }
